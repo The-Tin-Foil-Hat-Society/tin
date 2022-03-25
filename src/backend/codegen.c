e10c87c73d4081a94e3dc3e1b92ff62cf6a0d874
Alex <alex@gu3.me>
2022-03-25 17:26:38 +0000

    Refactor codegen so that it supports multiple strings - still a mess though

--- src/backend/codegen.c
+++ src/backend/codegen.c
@@ -1,25 +1,32 @@
+#include <stdarg.h>
+
 #include "codegen.h"
 #include "ast.h"
 
-#define write_instruction( ... ) fprintf(file, __VA_ARGS__)
+// Define string table
+#define STRING_TABLE_SIZE 1024
+char *string_table[STRING_TABLE_SIZE];
+int string_table_index = 0;
 
-void write_string_table( FILE* file, ast_node* node ) 
+// Define instruction table (list of strings)
+#define INSTRUCTION_TABLE_SIZE 4096
+char *instruction_table[INSTRUCTION_TABLE_SIZE];
+int instruction_table_index = 0;
+
+void add_instruction( const char* fmt, ... ) 
 {
-    for (int i = 0; i < node->children->size; i++)
-    {
-        ast_node* child = vector_get_item( node->children, i );
-
-        if (child->type == AstStringLit)
-        {
-            write_instruction( "str_0: \t.string \"%s\\n\"\n", child->value.string );
-            write_instruction( "\n" );
-        }
-
-        write_string_table( file, child );
-    }
+    va_list args;
+    va_start( args, fmt );
+    char* instruction = malloc(1024);
+    vsprintf( instruction, fmt, args );
+    va_end( args );
+    instruction_table[instruction_table_index++] = instruction;
 }
 
-void write_nodes( FILE* file, ast_node* node ) 
+#define add_string( str ) string_table[string_table_index++] = str
+#define write_to_file( ... ) fprintf(file, __VA_ARGS__)
+
+void walk_through_nodes( FILE* file, ast_node* node ) 
 {
     for (int i = 0; i < node->children->size; i++)
     {
@@ -27,50 +34,67 @@ void write_nodes( FILE* file, ast_node* node )
 
         if (child->type == AstPrint)
         {
-            write_instruction( "\t# Print node\n" );
-            write_instruction( "\tli a0, 4\n" );
-            write_instruction( "\tla a1, str_0\n" );
-            write_instruction( "\tecall\n" );
-            write_instruction( "\n" );
+
+            // String is a child of this node
+            ast_node* string_node = vector_get_item( child->children, 0 );
+            add_string( string_node->value.string );
+
+            add_instruction( "\t# Print node\n" );
+            add_instruction( "\tli a0, 4\n" );
+            add_instruction( "\tla a1, str_%d\n", string_table_index - 1 );
+            add_instruction( "\tecall\n" );
+            add_instruction( "\n" );
         }
 
-        write_nodes( file, child );
+        walk_through_nodes( file, child );
     }
 }
 
 bool codegen_generate(module* mod, ast_node* node, FILE* file) 
 {
-    write_instruction( "# \n" );
-    write_instruction( "# Auto-generated file \n" );
-    write_instruction( "# \n" );
+    // Walk through each node so that we know what we're writing
+    walk_through_nodes( file, node );
+
+    write_to_file( "# \n" );
+    write_to_file( "# Auto-generated file \n" );
+    write_to_file( "# \n" );
 
     //
     // Definitions
     //
-    write_instruction( ".globl __start\n" );
-    write_instruction( "\n" );
+    write_to_file( ".globl __start\n" );
+    write_to_file( "\n" );
 
     //
     // String table
     //
-    write_instruction( ".rodata\n" );
-    write_string_table( file, node );
+    write_to_file( ".rodata\n" );
+    // Write strings
+    for (int i = 0; i < string_table_index; i++)
+    {
+        write_to_file( "str_%d:\n", i );
+        write_to_file( "\t.string \"%s\\n\"\n", string_table[i] );
+    }
 
     //
     // Text section
     //
-    write_instruction( ".text\n" );
-    write_instruction( "\n" );
+    write_to_file( ".text\n" );
+    write_to_file( "\n" );
 
     //
     // Program entry point
     //
-    write_instruction( "__start:\n" );
-    write_nodes( file, node );
+    write_to_file( "__start:\n" );
+    // Write instructions
+    for (int i = 0; i < instruction_table_index; i++)
+    {
+        write_to_file( "%s", instruction_table[i] );
+    }
 
     //
     // Program exit point
     //
-    write_instruction( "\tli a0, 10\n" );
-    write_instruction( "\tecall\n" );
+    write_to_file( "\tli a0, 10\n" );
+    write_to_file( "\tecall\n" );
 }
