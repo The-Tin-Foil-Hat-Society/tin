65df1ca5c7620e540a99cdc888223c948f29f011
Alex <alex@gu3.me>
2022-03-25 17:46:54 +0000

    Delete build.bat & build.sh - use make instead

--- build.bat
+++
@@ -1,5 +0,0 @@
-mkdir generated 2>NUL
-mkdir build 2>NUL
-flex --header-file=generated/lex.yy.h --outfile=generated/lex.yy.c src/tin.l
-bison -d -b generated/parser src/tin.y
-gcc -D TIN_INTERPRETER -Isrc -Igenerated -Werror -g -O0 src/*.c generated/lex.yy.c generated/parser.tab.c -o build/tin.exe
