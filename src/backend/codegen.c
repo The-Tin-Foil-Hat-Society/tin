b29e00e466f74b6ffff92dc36b53fa3d4dc36352
Alex <alexnoir03@gmail.com>
2022-03-25 14:25:09 +0000

    Basic codegen file contents

--- src/backend/codegen.c
+++ src/backend/codegen.c
@@ -1,1 +1,6 @@
 #include "codegen.h"
+
+bool codegen_generate(module* mod, ast_node* node) 
+{
+    printf( "Hello from codegen\n" );
+}
--- src/backend/codegen.h
+++ src/backend/codegen.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include "ast.h"
+#include "module.h"
+#include "vector.h"
+
+bool codegen_generate(module* mod, ast_node* node);
