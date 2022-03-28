d044697d2fd9ebd933c1391ebf059b28c17dfeaa
Josh <me@joshl.io>
2022-03-28 17:17:01 +0100

    move around files

--- src/optimisation.h
+++ src/optimisation.h
@@ -3,4 +3,6 @@
 #include "ast.h"
 #include "module.h"
 
+int eval_condition(ast_node* condition, ast_node* node1, ast_node* node2)
+ast_node* find_conditions(ast_node* node)
 void optimize(module* mod, ast_node* node);
