1ef8719be0eda8f5800484b8b8e09c353afd962d
Andrejs Krauze <krauzeandrejs@gmail.com>
2022-04-13 23:46:44 +0100

    Implement `path_locate_file` to look for includes in PATH

--- src/utils/path.c
+++ src/utils/path.c
@@ -1,5 +1,6 @@
 #include "path.h"
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,8 +14,6 @@ char* path_trunc(char* path)
 
 char* path_join(int count, ...)
 {
-    // TODO: ensure there are delimiter between elements
-
     int len = 0;
     char* path = malloc(PATH_MAX_LENGTH);
     path[0] = '\0';
@@ -27,6 +26,14 @@ char* path_join(int count, ...)
         char* arg = va_arg(args, char*);
         if (arg != 0)
         {
+            // if there is no delimiter on either the previous or current element, and the current element isn't an extension, add the delimiter
+            if (len > 0 && path[len - 1] != PATH_DELIM_UNIX && path[len - 1] != PATH_DELIM_WIN && arg[0] != PATH_DELIM_UNIX && arg[0] != PATH_DELIM_WIN && arg[0] != '.')
+            {
+                path[len] = PATH_DELIM;
+                path[len + 1] = '\0';
+                len += 1;
+            }
+
             strcat(path, arg);
             len += strlen(arg);
         }
@@ -88,4 +95,47 @@ char* path_get_filename_wo_ext(char* path)
     }
 
     return path_trunc(filename);
+}
+char* path_locate_file(char* path, char* parent_dir)
+{
+    char* new_path = 0;
+
+    // try to find the file locally
+    if (parent_dir != 0)
+    {
+        new_path = path_join(2, parent_dir, path);
+    }
+    else
+    {
+        new_path = strdup(path);
+    }
+
+    FILE* file;
+    if (file = fopen(new_path, "rb"))
+	{
+        fclose(file);
+        return new_path;
+	}
+
+    // try to find the file in PATH
+    char* env_path = getenv("PATH");
+    if (env_path == 0)
+    {
+        return 0;
+    }
+    
+    // split the PATH variable into seperate paths
+    char* split_path = strtok(env_path, ENV_PATH_DELIM);
+    while (split_path != 0)
+    {
+        char* joined_path = path_join(2, split_path, path);
+        if (file = fopen(joined_path, "rb"))
+        {
+            new_path = joined_path;
+            break;
+        }
+        split_path = strtok(NULL, ENV_PATH_DELIM);
+    }
+
+    return new_path;
 }
