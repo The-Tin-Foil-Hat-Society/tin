8367ba6dcf06804745fa5c1249929a1aa7881990
Josh <me@joshl.io>
2022-04-15 17:20:07 +0100

    remove unnecessary comments

--- src/optimisation.c
+++ src/optimisation.c
@@ -31,99 +31,6 @@ int literals[4] = {
     AstFloatLit
 };
 
-/*
-0 - condition can't be evaluated
-1 - reference first node
-2 - reference second node
-*/
-/*int eval_condition(ast_node* condition, ast_node* node1, ast_node* node2)
-{
-    long long int value1 = node1->value.integer;
-    long long int value2 = node2->value.integer;
-
-    if (condition->type == AstEqual)
-    {
-        return value1 == value2 ? 1 : 2;
-    }
-    else if (condition->type == AstGreaterThan)
-    {
-        return value1 > value2 ? 1 : 2;
-    }
-    else if (condition->type == AstGreaterThanOrEqual)
-    {
-        return value1 >= value2 ? 1 : 2;
-    }
-    else if (condition->type == AstLessThan)
-    {
-        return value1 < value2 ? 1 : 2;
-    }
-    else if (condition->type == AstLessThanOrEqual)
-    {
-        return value1 <= value2 ? 1 : 2;
-    }
-
-    return 0;
-}
-
-
-
-ast_node* initial_iteration(ast_node* node, hashtable* symbols)
-{
-    for (size_t i = 0; i < node->children->size; i++)
-    {
-        ast_node* child = ast_get_child(node, i);
-
-        if (child->type == AstSymbol)
-        {
-            if (node->type != AstAssignment && node->type != AstDefinition)
-            {
-                variables[pointer] = child->value.symbol->name;
-                pointer++;
-
-                char* name = child->value.symbol->name;
-                hashtable_set_item(used_vars, name, name);
-            }
-        }
-
-        initial_iteration(child, symbols);
-    }
-}
-
-ast_node* find_conditions(ast_node* node)
-{   
-    if (node->type == AstIf)
-    {
-        ast_node* condition = ast_get_child(node, 0);
-
-        if (condition->children->size == 2)
-        {
-            ast_node* node1 = ast_get_child(condition, 0);
-            ast_node* node2 = ast_get_child(condition, 1);
-            
-            if (node1->type == AstIntegerLit && node2->type == AstIntegerLit)
-            {
-                int response = eval_condition(condition, node1, node2);
-
-                if (response != 0)
-                {
-                    return ast_get_child(node, response);
-                }
-            }
-        }
-    }
-
-    for (size_t i = 0; i < node->children->size; i++)
-    {
-        ast_node* scope = find_conditions(ast_get_child(node, i));
-        if (scope)
-        {
-            ast_set_child(node, i, scope);
-        }
-    }
-
-    return NULL;
-}*/
-
 ast_node* replace_if_statements(ast_node* node, bool determinable)
 {
     for (size_t i = 0; i < node->children->size; i++)
@@ -188,11 +95,6 @@ ast_node* remove_variables(ast_node* node)
         if (symbol) {
             char* name = symbol->value.symbol->name;
             void* item = hashtable_get_item(used_vars, name);
-
-            if (!item)
-            {
-                //printf(name);
-            }
         }
 
         remove_variables(ast_get_child(node, i));
@@ -215,19 +117,6 @@ bool compare_value(ast_node* node1, ast_node* node2)
 {
     data_type* left_dtype = ast_find_data_type(node1);
     data_type* right_dtype = ast_find_data_type(node2);
-
-    /*if (is_int(left_dtype) && is_int(right_dtype))
-    {
-        return (node1->value.integer == node2->value.integer);
-    }
-    else if (is_bool(left_dtype) && is_bool(right_dtype))
-    {
-        return (node1->value.boolean == node2->value.boolean);
-    }
-    else if (is_string(left_dtype) && is_string(right_dtype))
-    {
-        return (node1->value.string == node2->value.string);
-    }*/
 
     return (node1->value.integer == node2->value.integer
         && node1->value.boolean == node2->value.boolean
@@ -589,8 +478,6 @@ ast_node* evaluate_expression(ast_node* node, bool determinable)
             node->value.boolean = node1->value.boolean && node2->value.boolean;
             node->children = new_node->children;
             ast_get_child(node, 0)->parent = node;
-            //else if (node1->type == AstIntegerLit && node1->value.integer != 0)
-            //else if (node1->type == AstStringLit && strlen(node1->value.string) > 0)
         }
         else if (node->type == AstOr)
         {
@@ -599,11 +486,7 @@ ast_node* evaluate_expression(ast_node* node, bool determinable)
             node->value.boolean = node1->value.boolean || node2->value.boolean;
             node->children = new_node->children;
             ast_get_child(node, 0)->parent = node;
-            //else if (node1->type == AstIntegerLit && node1->value.integer != 0)
-            //else if (node1->type == AstStringLit && strlen(node1->value.string) > 0)
         }
-
-        //printf("%lld\n", node->value.integer);
     }
 
     return node;
@@ -619,30 +502,13 @@ ast_node* simplify_expression(ast_node* node, bool determinable)
         {
             ast_node* child = simplify_expression(ast_get_child(node, i), determinable);
             ast_set_child(node, i, child);
-
-            /*if (child->type == AstSymbol && determinable)
-            {
-                symbol* variable = ast_find_symbol(node, child->value.symbol->name);
-
-                if (is_int(variable->dtype) && variable->is_literal)
-                {
-                    ast_node* new_child = ast_new(AstIntegerLit);
-                    new_child->value.integer = variable->value.integer;
-                    
-                    ast_set_child(node, i, new_child);
-
-                    //printf("%lld, %lld\n", child->value.integer, variable->value.integer);
-                }
-            }*/
         }
 
         evaluate_expression(node, determinable);
-        //printf("%lld\n", node->value.integer);
     }
     else if (node->type == AstSymbol)
     {
-        symbol* variable = node->value.symbol;//ast_find_symbol(node, node->value.symbol->name);
-        //printf("%s: %d\n", variable->name, variable->value.integer);
+        symbol* variable = node->value.symbol;
 
         variable->variable_uses++;
 
@@ -721,30 +587,12 @@ ast_node* assign_variable(ast_node* node, bool determinable)
         ast_node* child = ast_get_child(node, 1);
         child = simplify_expression(child, determinable);
 
-        //printf("%d", (int)child->type);
-
-        /*if (child->type == AstIntegerLit)
-        {
-            ast_node* new_child = ast_new(AstIntegerLit);
-            new_child->value.integer = child->value.integer;
-            ast_set_child(node, 1, new_child);
-        }
-        else
-        {
-            variable->is_literal = false;
-        }*/
-
-        //("%s, %lld; ", variable->name, (int)child->value.integer);
-
         if (check_children(child, literals, sizeof(literals)) && variable->is_literal)
         {
-            //symbol* variable = ast_find_symbol(child, variable->name);
-
             if (is_int(variable->dtype))
             {
                 variable->value.integer = child->value.integer;
                 variable->is_assigned = true;
-                ////printf("%i", *((int*)variable->value));
             }
             else if (is_float(variable->dtype))
             {
@@ -755,14 +603,11 @@ ast_node* assign_variable(ast_node* node, bool determinable)
             {
                 variable->value.string = child->value.string;
                 variable->is_assigned = true;
-                //printf("%s", variable->value);
             }
             else if (is_bool(variable->dtype))
             {
                 variable->value.boolean = child->value.boolean;
-                //printf(variable->name);
                 variable->is_assigned = true;
-                //printf("%i", *((bool*)variable->value));
             }
         }
         else
@@ -779,12 +624,6 @@ ast_node* assign_variable(ast_node* node, bool determinable)
 ast_node* find_expressions(ast_node* node, bool determinable)
 {
     bool allow_scopes = false;
-
-    if (check_children(node, comparitive_nodes, sizeof(comparitive_nodes)))
-    {
-        //node = simplify_expression(node);
-        //printf("%lld\n", node->value.integer);
-    }
 
     if (node->type == AstAssignment)
     {
@@ -930,25 +769,6 @@ void optimize(module* mod, ast_node* node)
     {
         count++;
         children = vector_copy(node->children);
-
-        if (mod->module_store != 0 && mod->module_store->size > 0)
-        {
-            vector* module_vec = hashtable_to_vector(mod->module_store);
-            for (int i = 0; i < module_vec->size; i++)
-            {
-                module* mod_vector = vector_get_item(module_vec, i);
-                ast_node* mod_node = mod_vector->ast_root;
-                
-                for (size_t i = 0; i < mod_node->children->size; i++)
-                {
-                    ast_node* child = ast_get_child(mod_node, i);
-                    if (child->type == AstAssignment)
-                    {
-                        assign_variable(child, true);
-                    }
-                }
-            }
-        }
         
         for (size_t i = 0; i < node->children->size; i++)
         {
@@ -963,46 +783,14 @@ void optimize(module* mod, ast_node* node)
 
                 if (strcmp(declaration->value.symbol->name, "main") == 0)
                 {
-                    //ast_node* new_child =
                     reset_variables(node);
                     find_expressions(child, true);
                     replace_if_statements(child, true);
                     remove_assignments(node);
-                    //remove_assignments(node);
-
-                    /*if (mod->module_store != 0 && mod->module_store->size > 0)
-                    {
-                        vector* module_vec = hashtable_to_vector(mod->module_store);
-                        for (int i = 0; i < module_vec->size; i++)
-                        {
-                            module* mod_vector = vector_get_item(module_vec, i);
-                            ast_node* mod_node = mod_vector->ast_root;
-
-                            remove_assignments(mod_node);
-                            reset_variables(mod_node);
-                        }
-                    }*/
-                    //new_child = remove_variables(new_child);
-                    //ast_set_child(node, i, new_child);
-                    //child = new_child;
+                    
                     break;
                 }
             }
         }
     }
-
-    //remove_assignments(node);
-    //remove_variables(node);
-    
-    //used_vars = hashtable_new();
-    //hashtable* symbols = hashtable_new();
-
-    //initial_iteration(node, symbols);
-    //remove_variables(node);
-
-    // temporary function that removes basic if statements
-    //find_conditions(node);
-    // currently working on finding a sensible solution
-    // to pre-determining conditions where possible,
-    // which will make all functions here much more effective
 }
